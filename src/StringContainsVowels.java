2d738c5a6b366f4a81793eaae87bc7a63ef19bbf
Test-QA-Automation <testqaforautomation@gmail.com>
2023-12-31 13:56:27 +0530

    additional programs

---
+++ src/StringContainsVowels.java
@@ -0,0 +1,13 @@
+public class StringContainsVowels
+{
+    public static void main(String[] args)
+    {
+        System.out.println(stringContainsVowels("Abhishek")); // true
+        System.out.println(stringContainsVowels("TV")); // false
+    }
+
+    public static boolean stringContainsVowels(String input)
+    {
+        return input.toLowerCase().matches(".*[aeiou].*");
+    }
+}
