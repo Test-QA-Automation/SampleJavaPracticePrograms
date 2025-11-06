e529e00cc518dd877b4284b2cb3f38395ca971e2
Test-QA-Automation <testqaforautomation@gmail.com>
2025-11-06 15:26:31 +0530

    new interview code

---
+++ src/FindOccurenceOfSubStringWithinString.java
@@ -0,0 +1,23 @@
+public class FindOccurenceOfSubStringWithinString
+{
+    public static void main(String[] args)
+    {
+        String word = "ABCEDTGJLLABCDSWQABC";
+        String subString = "ABC";
+        System.out.println("Occurrences of " + subString + " inside " + word + " is : " + getOccurence(word,subString));
+    }
+
+    public static int getOccurence(String str, String subString)
+    {
+        int index = 0;
+        int occurence = 0;
+
+        while ((index = str.indexOf(subString, index)) != -1)
+        {
+            occurence++;
+            index += subString.length();
+        }
+
+        return occurence;
+    }
+}
